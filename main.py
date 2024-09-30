f5bfebf26d2930b58e77ac1887f0515ac8c12f82
Auston Mtabane <mtabane.auston@gmail.com>
2024-09-30 10:17:05 +0200

    made option_sys() to run through options and indicate with an arrow the selected option

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import os
 
-def option_sys(options):
-    index : int 
-    for i, opt in enumerate(options):
-        print(i,opt)
 
+def option_sys(options):
+    index = 0
+    ch = "---"
+    while ch != "p": # looping the option for choosing if use has not pressed 'p' for pick
+
+        for i, opt in enumerate(options):
+            
+            if i != index: 
+                print("  ",opt) #print two empty spaces before option chosen 
+            else:
+                print("> ",opt) #print arrow before option chosen 
+            
+        ch = input("") #collect input to proceed choosing
+        print(ch)
+
+        if ch == "": #in enter is pressed point to next option 
+            index += 1
+            if index == len(options):
+                index = 0
+
+    
+    return index
 
 
 data = ["Chicken", "Port", "Beef", "Fish"]
-
 option_sys(data)
+
