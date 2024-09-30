32ba00455ad1d1c9ff12f0750573be2212d35f05
Auston Mtabane <mtabane.auston@gmail.com>
2024-09-30 09:01:01 +0200

    added a function for options display

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+def option_sys(options):
+    index : int 
+    for i, opt in enumerate(options):
+        print(i,opt)
+
+
+
+data = ["Chicken", "Port", "Beef", "Fish"]
+
+option_sys(data)
