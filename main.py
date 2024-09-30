70b3a058fe5f00c014d1e3c6e7784296e7697ce5
Auston Mtabane <mtabane.auston@gmail.com>
2024-09-30 10:50:59 +0200

    added a doc string and screen clear together with some ascii colors

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
 
-
-def option_sys(options):
+"""
+This fuction is made to visualise the selection of a particular item in a list.
+press "ENTER" to proceed pointing to the next item in the lis 
+if you want to select the option that is indicated by the arrow press 'p' (pick) and then 'ENTER'
+then the fuction will return the index of that item in a list
+"""
+def option_sys(options,title_msg):
     index = 0
     ch = "---"
     while ch != "p": # looping the option for choosing if use has not pressed 'p' for pick
-
+        print("\033[1m",title_msg,"\033[0;37m: \n")
         for i, opt in enumerate(options):
             
             if i != index: 
                 print("  ",opt) #print two empty spaces before option chosen 
             else:
-                print("> ",opt) #print arrow before option chosen 
+                print("\033[1;34m> \033[0;37m",opt) #print arrow before option chosen 
             
         ch = input("") #collect input to proceed choosing
         print(ch)
@@ -21,10 +26,15 @@ def option_sys(options):
             if index == len(options):
                 index = 0
 
+        os.system("clear")
+
     
     return index
 
 
-data = ["Chicken", "Port", "Beef", "Fish"]
-option_sys(data)
+if __name__ == '__main__':
+    
+    os.system("clear")
+    data = ["Chicken", "Port", "Beef", "Fish"]
+    print(option_sys(data,"Pick your food"))
 
